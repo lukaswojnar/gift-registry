2f292d2bb4f484dbb4297cc86c228a09d1b45813
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-02 15:48:36 +0100

    Basic gift model

---
+++ wishlist/db/migrate/20160102144818_create_gifts.rb
@@ -0,0 +1,10 @@
+class CreateGifts < ActiveRecord::Migration
+  def change
+    create_table :gifts do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
