d2e61ba01aab20900800aff20e5974344eab6229
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-15 10:39:49 +0100

    Gift model and Gift Controller
    There is a problem during gift creation -> No route matches
    {:action=>"show", :controller=>"gift", :id=>nil} missing required keys:
    [:id]

--- wishlist/app/controllers/gift_controller.rb
+++ wishlist/app/controllers/gift_controller.rb
@@ -1,8 +1,51 @@
 # Controller for create, update and delete gifts
 class GiftController < ApplicationController
 
-  def new
-
+  def index
+    @gifts = Gift.all
   end
 
+  def new
+    @gift = Gift.new
+  end
+
+  def edit
+    @gift = Gift.find(params[:id])
+  end
+
+  def show
+    @gift = Gift.find(params[:id])
+  end
+
+  def create
+    @gift = Gift.new(gift_params)
+
+    if @gift.save
+      redirect_to @gift
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @gift = Gift.find(params[:id])
+
+    if @gift.update(gift_params)
+      redirect_to @gift
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @gift = Gift.find(params[:id])
+    @gift.destroy
+
+    redirect_to gift_path
+  end
+
+  private
+  def gift_params
+    params.require(:gift).permit(:title, :description, :price, :link)
+  end
 end
