190998d7a551eb0bdbcd143da3615527a97805c8
Jakub Macák <kubamacak@gmail.com>
2016-01-21 15:59:54 +0100

    Initial gift method (new, create)

--- wishlist/app/controllers/gifts_controller.rb
+++ wishlist/app/controllers/gifts_controller.rb
@@ -1,2 +1,23 @@
 class GiftsController < ApplicationController
+  def new
+    @gift = Gift.new
+    @list = List.find params[:list_id]
+  end
+
+  def create
+    @gift = Gift.new(gift_params)
+    @gift.list_id = params[:list_id]
+    @gift.save
+    redirect_to '/'
+  end
+
+  def index
+    @gifts = Gift.where(:list_id => params[:list_id])
+  end
+
+  private
+     def gift_params
+       params.require(:gift).permit(:title, :description)
+     end
+
 end
