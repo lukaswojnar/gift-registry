68beceee3fdc0cc67a2c23a0c338a0f793090edd
Jakub Macák <kubamacak@gmail.com>
2016-01-24 18:37:01 +0100

    Check if link has protocol

--- wishlist/app/models/gift.rb
+++ wishlist/app/models/gift.rb
@@ -1,4 +1,15 @@
 class Gift < ActiveRecord::Base
    validates :title, presence: true, length: { minimum: 3 }
    belongs_to :list
+
+   before_validation :smart_add_url_protocol
+
+   protected
+
+   def smart_add_url_protocol
+     unless self.link[/\Ahttp:\/\//] || self.link[/\Ahttps:\/\//]
+       self.link = "http://#{self.link}"
+     end
+   end
+
 end
