dcf0363e5b6bf52bc1c4a51d65a2ea5164d25569
Jakub Macák <kubamacak@gmail.com>
2016-01-25 07:53:13 +0100

    Search by code

--- wishlist/config/routes.rb
+++ wishlist/config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     resources :gifts
   end
 
+  post '/' => 'lists#search'
+
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 end
