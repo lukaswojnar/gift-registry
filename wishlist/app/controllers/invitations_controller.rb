2e3de9116fada21f676149b90b15e37ce2207a0b
Jakub Macák <kubamacak@gmail.com>
2016-01-27 12:37:02 +0100

    Simple invitation (initialization)

--- wishlist/app/controllers/invitations_controller.rb
+++ wishlist/app/controllers/invitations_controller.rb
@@ -1,2 +1,19 @@
 class InvitationsController < ApplicationController
+  def new
+    @list = List.find params[:list_id]
+  end
+
+  def create
+    email = params[:email]
+    @user = User.find_by email: email
+    @invitation = Invitation.new
+    @invitation.list_id = params[:list_id]
+    if @user
+      @invitation.invited_user_id = @user.id
+    else
+      @invitation.invited_user_id = User.invite!(:email => email)
+    end
+    @invitation.save
+    redirect_to controller: 'lists', action: 'index'
+  end
 end
--- wishlist/config/routes.rb
+++ wishlist/config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   resources :lists do
     resources :gifts
+    resources :invitations
   end
 
-  post '/' => 'welcome#search'
+  post '/invitations/create' => 'invitations#create'
 
   devise_for :users#, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 end
