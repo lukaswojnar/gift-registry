9e5cd88d7062babb14ad063755ba884b68fbb874
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-28 10:52:00 +0100

    List validations and error messages

--- wishlist/app/models/list.rb
+++ wishlist/app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3, maximum: 500,
+                                              too_long: "%{count} characters is the maximum allowed" }
+  validates :description, presence:  true, length: { minimum: 3, too_short: " must have at least 3 characters"}
+
   belongs_to :user
   has_many :gifts
   has_many :invitations
