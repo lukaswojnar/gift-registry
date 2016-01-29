3329491d9927fa3394d67ba2bd79f35b112cba67
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-29 09:42:03 +0100

    Notification controller - basic methods

--- wishlist/app/controllers/notifications_controller.rb
+++ wishlist/app/controllers/notifications_controller.rb
@@ -1,2 +1,41 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @notifications = current_user.notifications
+  end
+
+  def new
+    @list = List.find params[:list_id]
+    @notification = Notification.new
+  end
+
+  def create
+    @list = List.find params[:list_id]
+    @notification = Notification.new(notification_params)
+    @notification.user_id = current_user.id
+    @notification.list_id = @list.id
+
+    if @notification.save
+      flash[:success] = "Notification has been created."
+      redirect_to controller: 'lists', action: 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @notification = Notification.find(params[:id])
+    @notification.destroy
+    redirect_to lists_path
+    flash[:notice] = "Notification has been deleted."
+  end
+
+  private
+  def notification_params
+    params.require(:notification).permit(:date)
+  end
+
+  # do we need Update? user just can create and delete notification.
+
 end
--- wishlist/app/models/notification.rb
+++ wishlist/app/models/notification.rb
@@ -1,4 +1,10 @@
 class Notification < ActiveRecord::Base
   belongs_to :user, dependent: :destroy
   belongs_to :list, dependent: :destroy
+
+  validate :date_is_valid_datetime
+
+  def date_is_valid_datetime
+    errors.add(:date, 'must be a valid datetime') if ((DateTime.parse(happened_at) rescue ArgumentError) == ArgumentError)
+  end
 end
