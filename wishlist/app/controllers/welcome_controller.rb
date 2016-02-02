1be1f75ccbc394a3306c0bac25da2b5e7ac76529
Jakub Macák <kubamacak@gmail.com>
2016-02-02 06:53:45 +0100

    Remove unused action

--- wishlist/app/controllers/welcome_controller.rb
+++ wishlist/app/controllers/welcome_controller.rb
@@ -1,13 +1,4 @@
 class WelcomeController < ApplicationController
   def index
   end
-
-  def search
-    if params[:query].blank?
-      redirect_to(:back)
-    else
-      redirect_to '/search/'+params[:query]
-    end
-  end
-
 end
