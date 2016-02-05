f431d0689ffaf74295f523c9c92f23ae773a2c25
Jakub Macák <kubamacak@gmail.com>
2016-02-05 16:21:15 +0100

    Invitation mail

--- wishlist/app/mailers/notification_mailer.rb
+++ wishlist/app/mailers/notification_mailer.rb
@@ -7,4 +7,11 @@ class NotificationMailer < ApplicationMailer
     @gifts = Gift.where(list_id: @notification.list.id, assigned_user_id: @user.id)
     mail(to: @user.email, subject: 'Your event will be soon!')
   end
+
+  def invitation_email_registered_user(user, inv)
+    @user = user
+    @invitation = inv
+    mail(to: @user.email, subject: 'Your have been invited to an event!')
+  end
+
 end
