1c540720c385917ab72ff82998adbc29a3d82cf3
Lukáš Wojnar <lukas.wojnar@gmail.com>
2016-02-06 15:53:47 +0100

    Menu current item helper functionality + styling

--- wishlist/app/helpers/application_helper.rb
+++ wishlist/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # Module for styling selected page
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : nil
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
