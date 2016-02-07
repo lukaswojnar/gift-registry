0ef94865469abb51edbc1853438ed08d21b399d9
Lukáš Wojnar <lukas.wojnar@gmail.com>
2016-02-07 02:54:59 +0100

    Notifications now check whether they are missing and instructs user what to do next.

--- wishlist/app/controllers/notifications_controller.rb
+++ wishlist/app/controllers/notifications_controller.rb
@@ -2,7 +2,8 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.where(:user_id => current_user.id)
+    @notifications = current_user.notifications
+    @invitations = current_user.invitations
   end
 
   def new
