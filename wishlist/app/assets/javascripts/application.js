396ccf8599252f459e7c2c14713db2c0383238f7
Lukáš Wojnar <lukas.wojnar@gmail.com>
2016-02-11 12:20:00 +0100

    Image LightBox added

--- wishlist/app/assets/javascripts/application.js
+++ wishlist/app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require lightbox
 // DatePicker.js file is loaded automatically when it is stored in /app/assets/javascripts/ folder
 // require foundation-datetimepicker
 //= require_tree .
